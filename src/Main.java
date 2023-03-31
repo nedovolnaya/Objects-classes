7c8886c1b1211d6ac706d9665cb6cce88395802f
Unteryana <ishtar_v@bk.ru>
2023-03-31 19:36:47 +0300

    добавлены объекты класса Author

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Book ulysses = new Book("Улисс", 1920,new Author("Джеймс", "Джойс"));
+        Author joyce = new Author("Джеймс", "Джойс");
+        Book ulysses = new Book("Улисс", 1920, joyce);
         System.out.println("ulysses.bookName = " + ulysses.getBookName());
+        System.out.println("ulysses.getAuthor() = " + ulysses.getAuthor().getAuthorName() + " " +
+                ulysses.getAuthor().getAuthorSurname());
         System.out.println("ulysses.publishingYear = " + ulysses.getPublishingYear());
-        System.out.println("ulysses.getAuthor().getAuthorName() = " + ulysses.getAuthor().getAuthorName());
         ulysses.setPublishingYear(1921);
         System.out.println("ulysses.getPublishingYear() = " + ulysses.getPublishingYear());
         System.out.println();
 
-        Book annaKarenina = new Book("Анна Каренина", 1878,new Author("Лев", "Толстой") );
+        Author tolstoy = new Author("Лев", "Толстой");
+        Book annaKarenina = new Book("Анна Каренина", 1878, tolstoy);
         System.out.println("annaKarenina.bookName = " + annaKarenina.getBookName());
+        System.out.println("annaKarenina.author = " + annaKarenina.getAuthor().getAuthorName() +
+                annaKarenina.getAuthor().getAuthorSurname());
         System.out.println("annaKarenina.publishingYear = " + annaKarenina.getPublishingYear());
-        System.out.println("annaKarenina.author = " + annaKarenina.getAuthor().getAuthorName());
         annaKarenina.setPublishingYear(1879);
         System.out.println("annaKarenina.getPublishingYear() = " + annaKarenina.getPublishingYear());
         System.out.println();
